907e25237fa550d9e68483c4de01103c4c8c64d9
cuckata <vcvetomir@yahoo.com>
2025-01-28 01:21:23 +0200

    pipeline fix test

--- tf/vpc.tf
+++ tf/vpc.tf
@@ -41,7 +41,7 @@ resource "aws_subnet" "public" {
 
 #Create Elastic IP for NAT Gateway
 resource "aws_eip" "eip" {
-  domain = true
+  domain = vpc
 }
 
 #Create NAT Gateway
@@ -49,6 +49,9 @@ resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.eip.id
   subnet_id = aws_subnet.public[0].id
 
+  depends_on = [ 
+    aws_eip.eip
+   ]
   tags = {
     Name = "NAT Gateway for node group"
   }
